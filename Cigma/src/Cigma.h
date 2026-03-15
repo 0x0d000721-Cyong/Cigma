a9ca1562d43d5ef9427416c11c4d48bbfa01097a
Cyong <fta920048900@gmail.com>
2026-03-16 07:34:34 +0800

    Setup and Entrypoint

--- Cigma/src/Cigma.h
+++ Cigma/src/Cigma.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include<stdio.h>
+
 //For use by Cigma application
 
 #include"Cigma/Application.h"
--- Cigma/src/Cigma/EntryPoint.h
+++ Cigma/src/Cigma/EntryPoint.h
@@ -5,7 +5,6 @@
 extern Cigma::Application* Cigma::CreateApplication();
 
 int main(int argc, char** argv) {
-
 	auto app = Cigma::CreateApplication();
 	app->Run();
 	delete app;
